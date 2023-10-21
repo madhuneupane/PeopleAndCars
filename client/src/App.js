c4f8eabccb30e7edd700714c04182c15b2c4ed30
madhuneupane <nyoupanemadhu07@gmail.com>
2023-10-20 21:46:37 -0700

    feat: add apolo client provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
-
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import './App.css';
 import Title from './components/layout/Title';
 import AddPerson from './components/forms/AddPerson';
 import AddCar from './components/forms/AddCar';
+import People from './components/list/People';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+})
+
 function App() {
   return (
+    <ApolloProvider client={client}>
    <div className='App'>
     <Title />
     <AddPerson />
     <AddCar />
+    <People />
     </div>
+    </ApolloProvider>
   );
 }
 
